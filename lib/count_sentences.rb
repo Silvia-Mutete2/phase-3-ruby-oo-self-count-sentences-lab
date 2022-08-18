b1257abda79b85ee2ad07e8fa1c2c7097f84b8fa
Silvia-Mutete <silvia.mutete@student.moringaschool.com>
2022-08-18 08:03:35 +0300

    commit tree 75944bc0260e6fba8b096b9bf2af96794c54d310

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,34 @@
 require 'pry'
-
 class String
 
   def sentence?
-    
+    if self[-1] == "."
+      true
+    else
+      false
+    end
   end
 
   def question?
-
+    if self[-1] == "?"
+      true
+    else
+      false
+    end
   end
 
   def exclamation?
-
+    if self[-1] == "!"
+      true
+    else
+      false
+    end
   end
 
   def count_sentences
-
+    array = self.split(/[\.\!\?]/).filter do |sentence|
+      sentence != ""
+    end
+    array.count
   end
 end
